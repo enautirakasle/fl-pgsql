471285f0ceacede5d460905a5cff8f6c23ad3f04
Eñaut AA <enaut.irakasle@gmail.com>
2025-06-15 12:39:39 +0200

    dejar como  antes, sin registro

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,9 @@ class AdminPanelProvider extends PanelProvider
             ->login()
             ->colors([
                 'primary' => Color::Amber,
-            ])            
+            ])
+            // ->profile(isSimple: false)
+            // ->registration()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
